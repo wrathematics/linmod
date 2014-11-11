3282742f5be140dd243947420d1365924407f362
wrathematics <wrathematics@gmail.com>
2014-11-10 19:56:52 -0500

    better lm_fit R test

--- tests/lm_fit.r
+++ tests/lm_fit.r
@@ -3,14 +3,15 @@ library(linmod)
 set.seed(1234)
 nrhs <- 2
 
-m <- 10
-n <- 5
 
-#n <- 10
-#m <- 5
 
-x <- matrix(rnorm(m*n), m, n)
-y <- matrix(rnorm(m*nrhs), m, nrhs)
+test <- function(m, n, nrhs, offset=NULL, whichprint=NULL)
+{
+  x <- matrix(rnorm(m*n), m, n)
+  y <- matrix(rnorm(m*nrhs), m, nrhs)
+  
+  linmod:::lmfit_test(x, y, offset=NULL, TRUE, whichprint)
+}
 
 #x[, 5] <- x[, 4]
 #x[, 3] <- x[, 2]
@@ -19,16 +20,7 @@ y <- matrix(rnorm(m*nrhs), m, nrhs)
 #x[, 5] <- x[, 4] <- x[, 3] <- x[, 2] <- x[, 1]
 
 
-#stats::lm.fit(x, y)$qr
-#linmod::lm_fit(x, y)$qr
 
+test(m=10, n=5, nrhs=2, offset=NULL, whichprint="coefficients")
+test(m=2000, n=100, nrhs=2, offset=NULL, whichprint=NULL)
 
-
-#whichprint <- NULL
-whichprint <- c("coefficients")
-
-linmod:::lmfit_test(x, y, offset=NULL, TRUE, whichprint)
-#f(x, y, FALSE)
-
-#stats::lm.fit(x, y)
-#linmod::lm_fit(x, y)
